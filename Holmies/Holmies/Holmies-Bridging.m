6c8cf4f3ccace07bd61dffe646ccf9083034a072
Leonardo Koppe Malanski <leonardomalan>
2015-10-06 16:45:43 -0300

    zerando git
    Former-commit-id: aac051eaa1c62ba55dc59c7ecf23f20dd63e327a

--- Holmies/Holmies/Holmies-Bridging.m
+++
@@ -1,9 +0,0 @@
-//
-//  Holmies-Bridging.m
-//  Holmies
-//
-//  Created by Leonardo Geus on 03/09/15.
-//  Copyright (c) 2015 Leonardo Geus. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
