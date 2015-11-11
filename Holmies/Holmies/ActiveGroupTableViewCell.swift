c09592eb612eb525109bcf3fbd59cfc67f393bbb
Leonardo Geus <leonardodegeus@gmail.com>
2015-11-11 16:25:28 -0200

    feito imagens dos users por scroll
    Former-commit-id: d21f3a9b49bcf614c9697299f6ef6534b0104091

--- Holmies/Holmies/ActiveGroupTableViewCell.swift
+++ Holmies/Holmies/ActiveGroupTableViewCell.swift
@@ -8,27 +8,30 @@
 
 import UIKit
 
-class ActiveGroupTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class ActiveGroupTableViewCell: UITableViewCell, UICollectionViewDelegate {
 
     @IBOutlet weak var nameGroup: UILabel!
     @IBOutlet weak var numberLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var coloredSquare: UIView!
-
-
-    @IBOutlet weak var friendsCollection: UICollectionView!
+//
+//
+//    @IBOutlet weak var friendsCollection: UICollectionView!
     
+    @IBOutlet weak var scrollViewFriends: UIScrollView!
     
     var indexPathCell:Int!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         print("2")
-        indexPathCell = DataManager.sharedInstance.actualCell
-        DataManager.sharedInstance.actualCell++
-        if (indexPathCell == DataManager.sharedInstance.allGroup.count+1) {
-            indexPathCell = 0
+        DataManager.sharedInstance.linkGroupAndUserToSharer { (result) -> Void in
         }
+//        indexPathCell = DataManager.sharedInstance.actualCell
+//        DataManager.sharedInstance.actualCell++
+//        if (indexPathCell == DataManager.sharedInstance.allGroup.count+1) {
+//            indexPathCell = 0
+//        }
 
 //        friendsCollection.numberOfItemsInSection(1)
 //        let index = NSIndexPath(forItem: 0, inSection: 0)
@@ -42,41 +45,42 @@ class ActiveGroupTableViewCell: UITableViewCell, UICollectionViewDelegate, UICol
 
         // Configure the view for the selected state
     }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if !(DataManager.sharedInstance.allGroup[indexPathCell].users == nil) {
-                return DataManager.sharedInstance.allGroup[indexPathCell].users.count
-        }
-        else {
-            return 0
-        }
-
-    }
-    
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let userCell = friendsCollection.dequeueReusableCellWithReuseIdentifier("usercell", forIndexPath: indexPath) as! UsersInGroupsCollectionViewCell
-        
-        let id = DataManager.sharedInstance.allGroup[indexPathCell].users[indexPath.row].userID
-        print("cell = \(indexPathCell) cellCollection = \(indexPath.row)")
-        userCell.imageUser.image = DataManager.sharedInstance.findImage(id)
-        
-        
-        
-        return userCell
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 0
-    }
-    
-    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-        return 0
-    }
-
+//    
+//    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        if !(DataManager.sharedInstance.allGroup[indexPathCell].users == nil) {
+//                let group = DataManager.sharedInstance.allGroup[indexPathCell].users
+//                return DataManager.sharedInstance.allGroup[indexPathCell].users.count
+//        }
+//        else {
+//            return 0
+//        }
+//
+//    }
+//    
+//    
+//    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+//        let userCell = friendsCollection.dequeueReusableCellWithReuseIdentifier("usercell", forIndexPath: indexPath) as! UsersInGroupsCollectionViewCell
+//        
+//        let id = DataManager.sharedInstance.allGroup[indexPathCell].users[indexPath.row].userID
+//        print("cell = \(indexPathCell) cellCollection = \(indexPath.row)")
+//        userCell.imageUser.image = DataManager.sharedInstance.findImage(id)
+//        
+//        
+//        
+//        return userCell
+//    }
+//    
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+//    }
+//
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+//        return 0
+//    }
+//    
+//    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+//        return 0
+//    }
+//
     
 }
